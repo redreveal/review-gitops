090f5292c4b39b9759e2f0d30a9ed66cd36b53cf
Redon Berisha <rberisha@revealdata.com>
2024-08-14 14:42:08 +0200

    update dev

--- .github/actions/scripts/process_values.py
+++ .github/actions/scripts/process_values.py
@@ -16,11 +16,11 @@ def write_yaml(data, file_path):
 def process_versions(version_file_path, output_dir):
     versions_data = read_yaml(version_file_path)
 
-    # Extract defaults
+    # Extract defaults in ordder to generate the default versions for each components
     defaults = versions_data.get('defaults', {})
     default_values = {'services': {}, 'versions': {}}
 
-    # Add defaults for components
+    # Add defaults for components, this needs to be as dynamic as possible, because currently we have 3 components (review, processing, reveal_ai) but in the future we want this to be dynamacally generated
     for component, component_data in defaults.items():
         default_version = component_data.get('default', 'default_version')
         default_values['versions'][f"default_{component}"] = default_version
@@ -28,12 +28,12 @@ def process_versions(version_file_path, output_dir):
         for service, version in services.items():
             default_values['services'][service] = {'tag': version}
 
-    # Write the default values.yaml
+    # Write the default values.yaml which all non-overriden MSA's will be symmlinked to
     default_values_path = os.path.join(output_dir, 'default_values.yaml')
     write_yaml(default_values, default_values_path)
     print(f"Generated {default_values_path}")
 
-    # Process each MSA
+    # Process each MSA that have some overriden vallues
     msas = versions_data.get('msas', {})
     for msa, msa_data in msas.items():
         values = {
@@ -41,9 +41,8 @@ def process_versions(version_file_path, output_dir):
             'versions': dict(default_values['versions'])
         }
 
-        # Apply MSA-specific overrides for all components
+        # Get MSA-specific overrides for all components, this is supposed to be in MSA level
         for component, component_data in msa_data.items():
-            # Update versions
             for key, version in component_data.get('versions', {}).items():
                 if key in values['versions']:
                     values['versions'][key] = version
@@ -55,10 +54,10 @@ def process_versions(version_file_path, output_dir):
                     values['services'][service]['tag'] = version
                     print(f"Overriding {service} for MSA {msa} with version {version}")
 
-        # Output debug information before writing the file
+        # Random Redon Debug
         print(f"MSA {msa} values before writing to file: {values}")
 
-        # Generate the MSA-specific YAML file
+        # Random Redon Debug
         msa_file_path = os.path.join(output_dir, f"{msa}.yaml")
         print(f"Writing to {msa_file_path}")
         write_yaml(values, msa_file_path)
